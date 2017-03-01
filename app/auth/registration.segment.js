294c39fc470c96f93b48798fb11b59249f613c25
DESKTOP-JB3NHP5\Dima <dimasmoliar@gmail.com>
2017-03-01 11:32:51 +0200

    1. Add registration segment. 2. Add sign-in segment.

---
+++ app/auth/registration.segment.js
@@ -0,0 +1,18 @@
+(() => {
+
+    'use strict';
+
+    angular
+        .module('app')
+        .config(RegistrationSegment);
+
+    RegistrationSegment.$inject = ['$routeSegmentProvider', '$routeProvider'];
+    function RegistrationSegment($routeSegmentProvider, $routeProvider) {
+        $routeProvider.otherwise({redirectTo: '/'});
+        $routeSegmentProvider.options.autoLoadTemplates = true;
+
+        $routeSegmentProvider.when('/registration', 'Registration').segment('Registration', {
+            templateUrl: 'auth/registration.html'
+        });
+    }
+})();
---
+++ app/signin-form/signin.segment.js
@@ -0,0 +1,18 @@
+(() => {
+
+    'use strict';
+
+    angular
+        .module('app')
+        .config(SignInSegment);
+
+    SignInSegment.$inject = ['$routeSegmentProvider', '$routeProvider'];
+    function SignInSegment($routeSegmentProvider, $routeProvider) {
+        $routeProvider.otherwise({redirectTo: '/'});
+        $routeSegmentProvider.options.autoLoadTemplates = true;
+
+        $routeSegmentProvider.when('/signIn', 'signIn').segment('signIn', {
+            templateUrl: 'signin-form/signin-form.html'
+        });
+    }
+})();
