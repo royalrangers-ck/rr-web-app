37efd4d71efde3e8ed7892036b7d3ac66478c35e
Myroslaw Telychko <myroslaw.telychko@gmail.com>
2017-03-07 17:33:00 +0200

    Added few Registrarion and Login API endpoints

--- app/app.constants.js
+++ app/app.constants.js
@@ -1,4 +1,4 @@
-(function (){
+(function () {
 
     'use strict';
 
@@ -11,10 +11,26 @@
             API: '/api'
         };
 
-        constants.USER = constants.API + '/user';
-        constants.EMAIL = constants.API + '/email';
-        constants.LOGOUT = constants.API + '/auth/logout';
+        // /auth endpoind handle POST authentication request
+        // and return token if auth successful
+        constants.AUTH = constants.API + '/auth';
 
+        // /user - demo returns autorized user info
+        constants.USER = constants.API + '/user';
+
+        // Unsecured API maybe placed in /open/** path
+        constants.OPEN = constants.API + '/open';
+
+        // Registration endpoints
+        constants.REGISTRATION = constants.API + '/registration';
+        constants.CITIES = constants.REGISTRATION + '/cities';
+        constants.GROUPS = constants.REGISTRATION + '/groups';
+        constants.PLATOONS = constants.REGISTRATION + '/platoons';
+        constants.SECTIONS = constants.REGISTRATION + '/sections';
+
+        // constants.EMAIL = constants.API + '/email';
+        // constants.LOGOUT = constants.API + '/auth/logout';
         return constants;
     }
 })();
+
