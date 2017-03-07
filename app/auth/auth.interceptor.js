682eba48f24a09e6bddb70042f00662e4386cbc8
Myroslaw Telychko <myroslaw.telychko@gmail.com>
2017-03-07 18:58:06 +0200

    Added simple interceptor with redirection on home page, when 401

--- app/auth/auth.interceptor.js
+++ app/auth/auth.interceptor.js
@@ -1,13 +1,19 @@
-(function() {
+(function () {
     angular
         .module('app')
         .factory('AuthInterceptor', AuthInterceptor);
 
+    module.config(['$httpProvider', function ($httpProvider) {
+        $httpProvider.interceptors.push('AuthInterceptor');
+    }]);
+
     AuthInterceptor.$inject = ['$q', '$injector'];
+
     function AuthInterceptor($q, $injector) {
-        var checkAuth = function(response) {
+
+        let checkAuth = function (response) {
             if (response && response.status === 401) {
-                // do what we want
+                redirectToHomePage();
             }
         };
 
@@ -20,7 +26,8 @@
                 return config;
             },
 
-            requestError: function(rejection) {
+
+            requestError: function (rejection) {
                 return $q.reject(rejection);
             },
 
@@ -32,11 +39,52 @@
             },
 
             /* Revoke client authentication if 401 is received */
-            responseError: function(rejection) {
+            responseError: function (rejection) {
                 checkAuth(rejection);
 
                 return $q.reject(rejection);
             }
         };
+
+
     }
 })();
+
+// Make asynchronous operations in request interceptor
+//
+// module.factory('myInterceptor', ['$q', 'someAsyncService', function($q, someAsyncService) {
+//     var requestInterceptor = {
+//         request: function(config) {
+//             var deferred = $q.defer();
+//             someAsyncService.doAsyncOperation().then(function() {
+//                 // Asynchronous operation succeeded, modify config accordingly
+//                 ...
+//                 deferred.resolve(config);
+//             }, function() {
+//                 // Asynchronous operation failed, modify config accordingly
+//                 ...
+//                 deferred.resolve(config);
+//             });
+//             return deferred.promise;
+//         }
+//     };
+//
+//     return requestInterceptor;
+// }]);
+
+// Session Injector
+//
+// module.factory('sessionInjector', ['SessionService', function(SessionService) {
+//     var sessionInjector = {
+//         request: function(config) {
+//             if (!SessionService.isAnonymus) {
+//                 config.headers['x-session-token'] = SessionService.token;
+//             }
+//             return config;
+//         }
+//     };
+//     return sessionInjector;
+// }]);
+// module.config(['$httpProvider', function($httpProvider) {
+//     $httpProvider.interceptors.push('sessionInjector');
+// }]);
