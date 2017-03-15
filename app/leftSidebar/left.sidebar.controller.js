73bec7980bd9b32da50c0529d5f5350184601673
zpawn <ilia.makarov@me.com>
2017-03-15 20:49:46 +0200

    RR-30 Create left navbar for app

---
+++ app/leftSidebar/left.sidebar.controller.js
@@ -0,0 +1,72 @@
+(() => {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('sidebarCtrl', sidebarCtrl);
+
+    function sidebarCtrl($scope) {
+
+        $scope.leftSidebarSchema = [{
+            name: 'Система досягнень',
+            route: '',
+            submenu: [
+                {
+                    name: 'Початківці',
+                    route: ''
+                }, {
+                    name: 'Відкривачі',
+                    route: ''
+                }, {
+                    name: 'Слідопити',
+                    route: ''
+                }, {
+                    name: 'Рейнджери',
+                    route: ''
+                }, {
+                    name: 'Командири',
+                    route: ''
+                }
+            ]
+        }, {
+            name: 'Мої нагороди',
+            route: '',
+            submenu: [
+                {
+                    name: 'Медалі',
+                    route: ''
+                }, {
+                    name: 'Планки',
+                    route: ''
+                }, {
+                    name: 'За табори',
+                    route: ''
+                }, {
+                    name: 'За походи',
+                    route: ''
+                }, {
+                    name: 'Зірки',
+                    route: ''
+                }, {
+                    name: 'Чверть/рік',
+                    route: ''
+                }, {
+                    name: 'Начання',
+                    route: ''
+                }
+            ]
+        }, {
+            name: 'Виконую',
+            route: ''
+        }, {
+            name: 'Адмініструю',
+            route: ''
+        }, {
+            name: 'Мій загін',
+            route: ''
+        }, {
+            name: 'Мої друзі',
+            route: ''
+        }];
+    }
+})();
--- app/static/js/footer.js
+++
@@ -1,9 +0,0 @@
-(function ($) {
-    'use strict';
-
-    $('.rra-scroll-to-top').click(function () {
-        $('html,body').animate({
-            scrollTop: 0
-        }, 250);
-    });
-}(jQuery));
