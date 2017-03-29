10f8def46e290d1a7f0af4d842417dc995c1ea94
zpawn <ilia.makarov@me.com>
2017-03-29 12:00:14 +0300

    RR-43 Get user info: refactoring

--- app/app.controller.js
+++ app/app.controller.js
@@ -12,18 +12,14 @@
 
         let req = {
             method: 'GET',
-            url: '/api/user',
-            headers: {
-                'Authorization': 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJlbWFpbDFAbWFpbC50ZXN0IiwiYXVkaWVuY2UiOiJ3ZWIiLCJjcmVhdGVkIjoxNDkwNjQ1MjkyOTc4LCJleHAiOjE0OTEyNTAwOTJ9.Kd3F9HHVo38UAO7D0LgLWlCvGIetEA0LTxJe8WpfiVUguMZIY3NjYpU5soKbIkSrRe6wGZqA9ZRy8avQp3eDUQ'
-            }
+            url: '/api/user'
         };
 
         $http(req).then((res) => {
-
             $log.debug('<== userInfoResponse:', res);
 
             if (res.data.success) {
-                vm.userInfo = res.data.data;
+                vm.currentUser = res.data.data;
             }
         });
     }
