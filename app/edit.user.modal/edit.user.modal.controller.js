40bcb96509389410803cbe908e59882ca02de69f
Vasyl Dykun <wasyldykun@gmail.com>
2017-04-08 20:31:45 +0300

    First version

---
+++ app/edit.user.modal/edit.user.modal.controller.js
@@ -0,0 +1,133 @@
+(() => {
+
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('EditUserModalController', EditUserModalController);
+
+    EditUserModalController.$inject = ['$log', 'growl', '$uibModalInstance', 'EditUserModalService', '$routeSegment',  '$rootScope'];
+    function EditUserModalController($log, growl, $uibModalInstance, EditUserModalService, $routeSegment, $rootScope) {
+        const vm = this;
+
+        vm.currentUser = $rootScope.currentUser;
+        vm.changeGroup = changeGroup;
+        vm.changePlatoon = changePlatoon;
+        vm.setSections = setSections;
+        vm.updateUser = updateCurrentUser;
+        vm.close = close;
+
+        activate();
+
+        ///
+
+        function activate() {
+            $log.debug('Init modal window...')
+            setCities(vm.currentUser.countryId);
+            setGroups(vm.currentUser.cityId);
+            setPlatoons(vm.currentUser.groupId);
+            setSections(vm.currentUser.platoonId);
+            setRanks();
+            $log.debug('User loaded to modal window:', vm.currentUser);
+        }
+
+        function updateUser() {
+            close();
+            let valuesToSend = {
+                firstName: vm.currentUser.firstName,
+                lastName: vm.currentUser.lastName,
+                gender: vm.currentUser.gender,
+                userAgeGroup: vm.currentUser.userAgeGroup,
+                telephoneNumber: vm.currentUser.telephoneNumber,
+                birthDate: vm.currentUser.birthDate.getTime(),
+                countryId: vm.currentUser.countryId,
+                cityId: vm.currentUser.cityId,
+                groupId: vm.currentUser.groupId,
+                platoonId: vm.currentUser.platoonId,
+                sectionId: vm.currentUser.sectionId,
+                userRank: vm.currentUser.userRank
+            };
+            EditUserModalService.updateUser({ userId: vm.currentUser.id }, valuesToSend,
+                (res) => {
+                    if (res.success) {
+                        growl.info('Дані оновлено');
+                        $routeSegment.chain[0].reload();
+                    } else {
+                        growl.error('Помилка:' + res.data.message);
+                    }
+                });
+        }
+
+        function setCities(countryId) {
+            if (countryId == null) return [];
+            EditUserModalService.city({ countryId: countryId }).$promise.then((res) => {
+                if (res.success) {
+                    vm.cities = res.data;
+                    $log.debug('Set citys list: ', res.data);
+                }
+            });
+        }
+
+        function setGroups(cityId) {
+            if (cityId == null) return [];
+            EditUserModalService.group({ cityId: cityId }).$promise.then((res) => {
+                if (res.success) {
+                    vm.groups = res.data;
+                    $log.debug('Set groups list: ', res.data);
+                }
+            });
+        }
+
+        function setPlatoons(groupId) {
+            if (groupId == null) return [];
+            EditUserModalService.platoon({ groupId: groupId }).$promise.then((res) => {
+                if (res.success) {
+                    vm.platoons = res.data;
+                    $log.debug('Set platoons list: ', res.data);
+                }
+            });
+        }
+
+        function setSections(platoonId) {
+            if (platoonId == null) return [];
+            EditUserModalService.section({ platoonId: platoonId }).$promise.then((res) => {
+                if (res.success) {
+                    vm.sections = res.data;
+                    $log.debug('Set sections list: ', res.data);
+                }
+            });
+        }
+
+        function setRanks() {
+            EditUserModalService.rank().$promise.then((res) => {
+                if (res.success) {
+                    vm.ranks = res.data;
+                    $log.debug('Set ranks list: ', res.data);
+                }
+            });
+        }
+
+        //Special function for view to correct change group
+        function changeGroup(cityId) {
+            setGroups(cityId);
+            vm.platoons = [];
+            vm.sections = [];
+        }
+
+        //Same, but to correct change platoon
+        function changePlatoon(groupId) {
+            setPlatoons(groupId);
+            vm.sections = [];
+        }
+
+        function close(data) {
+            $uibModalInstance.close(data);
+        }
+
+    }
+})();
+
+
+
+
+
--- app/utils/modal.dialog.service.js
+++ app/utils/modal.dialog.service.js
@@ -11,6 +11,7 @@
 
         this.profileModal = profileModal;
         this.approveCurrentUserModal = approveCurrentUserModal;
+        this.editUserModal = editUserModal;
 
         function profileModal (_currentAchieve) {
 
@@ -40,6 +41,17 @@
                         return _currentUser;
                     }
                 }
+            });
+        }
+
+        function editUserModal() {
+            
+            return $uibModal.open({
+                animation: true,
+                templateUrl: 'edit.user.modal/edit.user.modal.html',
+                controller: 'EditUserModalController',
+                controllerAs: 'vm',
+                size: 'lg'
             });
         }
     }
