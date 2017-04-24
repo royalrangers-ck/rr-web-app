2c64efa68f1b22b8718124fc8d7ee76e4fd697be
zpawn <ilia.makarov@me.com>
2017-04-24 20:27:59 +0300

    Refactoring: minor changes

--- landing/static/js/navbar-scrolling.js
+++ landing/static/js/navbar-scrolling.js
@@ -4,13 +4,13 @@
  */
 
 //this script enable pop-up menu
-$(window).scroll(function() {
-    let navbar = $(".lph-nav");
-    if ($(window).scrollTop() > 500) {
-        navbar.addClass('lph-nav_fixed_top');
-        navbar.removeClass('lp-header__curve');
-    } else {
-        navbar.removeClass('lph-nav_fixed_top');
-        navbar.addClass('lp-header__curve');
-    }
-});
+// $(window).scroll(function() {
+//     let navbar = $(".lph-nav");
+//     if ($(window).scrollTop() > 500) {
+//         navbar.addClass('lph-nav_fixed_top');
+//         navbar.removeClass('lp-header__curve');
+//     } else {
+//         navbar.removeClass('lph-nav_fixed_top');
+//         navbar.addClass('lp-header__curve');
+//     }
+// });
