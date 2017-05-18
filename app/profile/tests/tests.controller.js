383d996b814a6b36802aca0790f11ad90e47e654
zpawn <ilia.makarov@me.com>
2017-05-18 16:38:06 +0300

    Finish tests page

--- app/profile/tests/tests.controller.js
+++ app/profile/tests/tests.controller.js
@@ -11,77 +11,65 @@
         const vm = this;
 
         vm.tests = getTests();
-
-        vm.profileModal = function (testId, color) {
-            AppModalService.profileModal(findTest(testId, color), 'test');
-        };
-
-        vm.isInProgress = function (testId, color) {
-            let currentTest = findTest(testId, color);
-            return currentTest.action == 'inProgress';
-        };
-
-        vm.isNotGet = function (testId, color) {
-            let currentTest = findTest(testId, color);
-            return currentTest.action == 'notGet' ? 'not-get' : '';
-        };
+        vm.profileModal = profileModal;
+        vm.newTestModal = newTestModal;
 
         activate();
 
         ///
 
-        function activate() {
-            $log.debug('Init ProfileTests Controller ...');
-        }
+        function activate() {}
 
         function getTests () {
             $http.get(Endpoints.TEST).then((res) => {
                 if (res.data.success) {
-                    // ToDo.zpawn: refactor that
-                    vm.tests = res.data.data.length !== 0 ? res.data.data.length : getDemoTests();
+                    vm.tests = normalizeStructureTests(res.data.data);
                 }
-                $log.debug('==> testResponse:', res);
             });
         }
 
-        function findTest (testId, color) {
-            return vm.tests[color].find((test) => {
-                return test.id === testId;
+        /**
+         * Structure:
+         * {
+         *      testType: {
+         *          name: 'testTypeName',
+         *          list: [ ... ]
+         *      },
+         *      testType: {
+         *          name: 'testTypeName',
+         *          list: [ ... ]
+         *      },
+         *      ...
+         * }
+         */
+        function normalizeStructureTests (denormalizeTests) {
+            return denormalizeTests.reduce((tests, test) => {
+
+                if (typeof tests[test.testType] === 'undefined') {
+                    tests[test.testType] = {
+                        name: test.testType,
+                        list: []
+                    };
+                }
+
+                tests[test.testType].list.push(test);
+
+                return tests;
+            }, {});
+        }
+
+        function profileModal (testId, testType) {
+            AppModalService.profileModal(findTest(testId, testType), 'test');
+        }
+
+        function findTest (id, type) {
+            return vm.tests[type].list.find((test) => {
+                return test.id === id;
             });
         }
 
-        // ToDo.zpawn: remove that
-        function getDemoTests () {
-            return {
-                blue: [
-                    { id: 1, name: 'Blue Tests 1' },
-                    { id: 2, name: 'Blue Tests 2', action: 'inProgress' },
-                    { id: 3, name: 'Blue Tests 3', action: 'notGet' },
-                    { id: 4, name: 'Blue Tests 4', action: 'notGet' },
-                    { id: 5, name: 'Blue Tests 5', action: 'notGet' }
-                ],
-                yellow: [
-                    { id: 6, name: 'Yellow Tests 1' },
-                    { id: 7, name: 'Yellow Tests 2' },
-                    { id: 8, name: 'Yellow Tests 3', action: 'inProgress' },
-                    { id: 9, name: 'Yellow Tests 4', action: 'notGet' },
-                    { id: 10, name: 'Yellow Tests 5', action: 'notGet' }
-                ],
-                green: [
-                    { id: 11, name: 'Green Tests1' },
-                    { id: 12, name: 'Green Tests2' },
-                    { id: 13, name: 'Green Tests3' },
-                    { id: 14, name: 'Green Tests4', action: 'inProgress' },
-                    { id: 15, name: 'Green Tests5', action: 'notGet' }
-                ],
-                cyan: [
-                    { id: 16, name: 'Cyan Tests 1' },
-                    { id: 17, name: 'Cyan Tests 2' },
-                    { id: 18, name: 'Cyan Tests 3', action: 'inProgress' },
-                    { id: 19, name: 'Cyan Tests 4', action: 'notGet' },
-                    { id: 20, name: 'Cyan Tests 5', action: 'notGet' }
-                ]
-            };
+        function newTestModal () {
+            AppModalService.testFormModal();
         }
     }
 })();
