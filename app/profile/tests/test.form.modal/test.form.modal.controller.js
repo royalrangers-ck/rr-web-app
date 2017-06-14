85b634920e3afd9be67408533f307717ca181b76
zpawn <ilia.makarov@me.com>
2017-06-14 20:38:42 +0300

    Implement 'Create new test' functionality on app

--- app/profile/tests/test.form.modal/test.form.modal.controller.js
+++ app/profile/tests/test.form.modal/test.form.modal.controller.js
@@ -6,13 +6,39 @@
         .module('app')
         .controller('TestFormModalController', TestFormModalController);
 
-    function TestFormModalController ($uibModalInstance) {
+    function TestFormModalController ($uibModalInstance, $http, Endpoints, growl, $location, $log) {
         const vm = this;
 
         vm.close = close;
+        vm.submit = submit;
+
+        ////
 
         function close(data) {
             $uibModalInstance.close(data);
+        }
+
+        function submit () {
+
+            if (!vm.form || vm.form.$invalid) {
+                return vm.form.$submitted = false;
+            }
+
+            let data = {
+                name: vm.test.name,
+                shortDescription: vm.test.shortDescription,
+                description: vm.test.description,
+                testType: vm.test.testType
+            };
+
+            $http.post(Endpoints.ACHIEVEMENTS_TEST, data).then((res) => {
+                if (res.data.success) {
+                    growl.success(res.data.data.message);
+                    $uibModalInstance.close();
+                } else {
+                    growl.error(res.data.data.message);
+                }
+            });
         }
     }
 })();
