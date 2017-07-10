7ebd9f94aa5a55e24c258a596cbbfc4edda5e9a5
Vasyl Dykun <wasyldykun@gmail.com>
2017-07-10 16:04:47 +0300

    implemented browserSync in our project

---
+++ gulp.tasks/browserSync/browserSync.js
@@ -0,0 +1,39 @@
+const gulp = require('gulp'),
+    bSync = require('browser-sync'),
+    http_proxy = require('http-proxy-middleware'),
+
+    proxy_conf = {
+        target: 'http://172.16.1.1:8080',
+        changeOrigin: true,               // needed for virtual hosted sites 
+        ws: true
+    },
+
+    api_link = http_proxy('/api', proxy_conf),
+
+    config = {
+        server: {
+            baseDir: './landing',
+            routes: {
+                '/app': 'app',
+                '/admin': 'admin'
+            },
+            middleware: [api_link]
+        },
+        tunnel: true,
+        host: 'localhost',
+        port: 9000,
+        logPrefix: "Frontend_Royal-Rangers"
+    },
+    browserSync = {
+        start: bs_start
+    };
+
+module.exports = browserSync;
+
+function bs_start () {
+    bSync(config);
+}
+
+gulp.task('bs_start', function () {
+    bSync(config);
+});
