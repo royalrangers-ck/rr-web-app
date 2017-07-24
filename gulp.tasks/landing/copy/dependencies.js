9e5f59db64e1f2e2904d9360577e7b293894dde2
Myroslaw Telychko <myroslaw.telychko@gmail.com>
2017-07-24 15:54:46 +0300

    Implement yarn on landing - Remove bower, move his dependecies in npm - Remove overkill double injection angular-ui-bootstrap - Change routes in gulp tasks - Change routes in app.scss injections

--- gulp.tasks/landing/copy/dependencies.js
+++ gulp.tasks/landing/copy/dependencies.js
@@ -15,19 +15,18 @@ module.exports = dependencies;
 function landing() {
 
     const src = [
-        'bower_components/jquery/dist/jquery.min.js',
-        'bower_components/bootstrap-sass/assets/javascripts/bootstrap.min.js',
-        'bower_components/angular/angular.min.js',
-        'bower_components/angular-route/angular-route.min.js',
-        'bower_components/angular-resource/angular-resource.min.js',
-        'bower_components/angular-animate/angular-animate.js',
-        'bower_components/angular-route-segment/build/angular-route-segment.js',
-        'bower_components/angular-growl-v2/build/angular-growl.min.js',
-        'bower_components/ngstorage/ngStorage.min.js',
-        'bower_components/moment/min/moment.min.js',
-        'bower_components/angular-bootstrap/ui-bootstrap.min.js',
-        'bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js',
-        'bower_components/angular-ui-mask/dist/mask.min.js',
+        'node_modules/jquery/dist/jquery.min.js',
+        'node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js',
+        'node_modules/angular/angular.min.js',
+        'node_modules/angular-route/angular-route.min.js',
+        'node_modules/angular-resource/angular-resource.min.js',
+        'node_modules/angular-animate/angular-animate.min.js',
+        'node_modules/angular-route-segment/build/angular-route-segment.min.js',
+        'node_modules/angular-growl-v2/build/angular-growl.min.js',
+        'node_modules/ngstorage/ngStorage.min.js',
+        'node_modules/moment/min/moment.min.js',
+        'node_modules/angular-ui-bootstrap/dist/ui-bootstrap-tpls.js',
+        'node_modules/angular-ui-mask/dist/mask.min.js',
 
         'landing/static/js/*.js',
         '!landing/static/js/google-maps.js'
